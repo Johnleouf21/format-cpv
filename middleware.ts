806f5638842e4c19325439be7c7d0a219d2ee1fe
Johnleouf21 <charlot.jonathan@hotmail.fr>
2026-03-20 15:19:14 +0100

    fix: manifest in middleware

--- middleware.ts
+++ middleware.ts
@@ -82,6 +82,6 @@ function getDefaultRoute(role: string): string {
 
 export const config = {
   matcher: [
-    '/((?!_next/static|_next/image|favicon.ico|public/).*)',
+    '/((?!_next/static|_next/image|favicon.ico|manifest|robots|public/).*)',
   ],
 }
